85b4a086ab57a8ed088b38f99d6e1a6f15b7e25d
Adam Morley <adam@Adams-Mac-mini.local>
2018-08-13 16:02:17 -0700

    add some tests to ensure insert path works

--- avlTree_test.go
+++ avlTree_test.go
@@ -2,4 +2,13 @@ package avlTree
 
 import "testing"
 
-
+func TestInsert(test *testing.T) {
+    var tree *avlTree = New()
+    tree.Insert(1)
+    tree.Insert(1)
+    tree.Insert(2)
+    test.Error(tree.Size())
+    test.Error(tree.Search(1))
+    test.Error(tree.Search(2))
+    test.Error(tree.Search(3))
+}
